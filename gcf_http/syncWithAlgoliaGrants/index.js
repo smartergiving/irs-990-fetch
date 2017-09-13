f8d12d14c551394ca6b09366d3ce4d0b98943553
Chad Kruse <chadokruse@users.noreply.github.com>
2017-09-13 15:20:51 -0500

    Update Algolia sync functions

--- gcf_http/syncWithAlgoliaGrants/index.js
+++ gcf_http/syncWithAlgoliaGrants/index.js
@@ -1,4 +1,4 @@
-// gcloud beta functions deploy syncWithAlgolia --stage-bucket bucketname --trigger-http
+// gcloud beta functions deploy functionName --stage-bucket bucketname --trigger-http
 const algoliasearch = require('algoliasearch');
 const secrets = require('./secrets');
 
@@ -12,18 +12,18 @@ const index = client.initIndex(indexName);
 // MongoDB
 const Db = require('mongodb').Db;
 const Server = require('mongodb').Server;
-const db = new Db('grantmakers', new Server(secrets.gce.host, '27017'));
+const db = new Db('irs', new Server(secrets.gce.host, '27017'));
 
 exports.syncWithAlgolia = function syncWithAlgolia(req, res) {
   // Open a db connection
-  db.open(function(e) {
-    if (e) throw new Error(e);
+  db.open(function(oErr) {
+    if (oErr) throw new Error(oErr);
 
-    db.admin().authenticate(secrets.gce.user, secrets.gce.password, function(err, result) {
+    db.admin().authenticate(secrets.gce.user, secrets.gce.password, function(aErr, result) {
       if (result) {
         // Get the collection
-        db.collection('algolia', function(error, collection) {
-          if (error) throw new Error(err);
+        db.collection('grants', function(gErr, collection) {
+          if (gErr) throw new Error(gErr);
 
           // Get the collection count
           collection.count()
@@ -33,11 +33,11 @@ exports.syncWithAlgolia = function syncWithAlgolia(req, res) {
               let batch = [];
 
               // Iterate over the whole collection using a cursor
-              return collection.find().forEach(function(doc) {
+              return collection.find().forEach(function loopThroughCollection(doc) {
                 // Remove unnecessary fields
                 delete doc._id;
                 
-                // Add doc to array
+                // Add doc to batch array
                 batch.push(doc);
                 ++processedCount;
 
